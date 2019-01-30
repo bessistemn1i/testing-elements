a0bb918fae410527770a3934c379b6bcc25bb92a
bessistemn1i <eduard.zhulega@yandex.ru>
2019-01-30 16:30:02 +0200

    fix names into more readable

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 import Generator from "./modules/Proto";
+import Oop from "./modules/Oop";
+import Canvas from "./modules/Canvas";
 
 const gen = new Generator();
+//const oop = new Oop();
+const canvas = new Canvas();
