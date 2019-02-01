b3dc7f9288e37f244002864beebc5290176311d7
bessistemn1i <eduard.zhulega@yandex.ru>
2019-02-01 16:58:26 +0200

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import Generator from "./modules/Proto";
 import Oop from "./modules/Oop";
 import Canvas from "./modules/Canvas";
 
-const gen = new Generator();
+const gen = new Generator({
+    amountOfSlides: 3
+});
 //const oop = new Oop();
 const canvas = new Canvas();
--- src/js/modules/Proto.js
+++ src/js/modules/Proto.js
@@ -1,5 +1,6 @@
 class Generator {
-    constructor() {
+    constructor(obj) {
+        this.amountOfSlides = obj.amountOfSlides;
         this.events();
     }
 
@@ -12,14 +13,15 @@ class Generator {
         let distanceBetweenSlides;
         let maxDistanceFromStart;
         let minDistanceFromStart;
-        
+        let amountOfSlides = this.amountOfSlides;
         function locateSlides() {
             let slideWidthInTheString = (window.getComputedStyle(slides[0])).getPropertyValue('max-width');
             let slideWidthInProcent = +(slideWidthInTheString.match(/\d/g).join(''));
-            slideGapInProcent = Math.round(((100 - (slideWidthInProcent * 3)) / 2) * 100 / slideWidthInProcent);
+            slideGapInProcent = Math.round(((100 - (slideWidthInProcent * amountOfSlides)) / (amountOfSlides - 1)) * 100 / slideWidthInProcent);
             distanceBetweenSlides = (slideGapInProcent + 100);
             maxDistanceFromStart = (slideGapInProcent + 100) * (slides.length - 1);
             minDistanceFromStart = -(slideGapInProcent + 100);
+            
             for(let i = 0; i < slides.length; i++) {
             const slide = slides[i];
             distanceFromStart = (slideGapInProcent + 100) * i;
@@ -27,22 +29,6 @@ class Generator {
             }
         };
         locateSlides();
-        function debounce(func, wait = 300, immediate = true) {
-            let timeout;
-            return function () {
-                let context = this,
-                    args = arguments;
-                
-                let later = function () {
-                    timeout = null;
-                    if(!immediate) func.apply(context, args);
-                };
-                let callNow = immediate && !timeout;
-                clearTimeout(timeout);
-                timeout = setTimeout(later, wait);
-                if(callNow) func.apply(context, args);
-            };
-        };
 
         function debounceOnResize(func) {
             let timer;
@@ -51,6 +37,7 @@ class Generator {
                 timer = setTimeout(func, 400, event);
             }
         }
+
         window.addEventListener('resize', debounceOnResize(locateSlides));
 
         function moveSlides() {
@@ -72,8 +59,9 @@ class Generator {
                         sliderContainer.firstElementChild.style.transform = `translateX(0%)`;
                         sliderContainer.firstElementChild.style.opacity = 1;
                         slide.style.transform = `translateX(${locationOnClickFromStart}%)`;
-                        slide.style.transition = `all 0.2s ease-in-out`;
+                        slide.style.transition = `all 0.25s ease-in-out`;
                     }, 0);
+                    
                 });
             }
 
@@ -84,27 +72,31 @@ class Generator {
                     let locationFromStart = slide.style.transform;
                     const locationFromStartInDigit = +(locationFromStart.match(/[\d]/g).join(''));
                     let locationOnClickFromStart = locationFromStartInDigit - distanceBetweenSlides;
+                    
                     if(locationOnClickFromStart < 0) {
                         locationOnClickFromStart = minDistanceFromStart;
-                        slide.style.transition = `transform 0.2s ease-in-out`;
-                        slide.style.transform = `translateX(0%)`;
-                        setTimeout(() => {
-                            slide.style.transform = `translateX(${locationOnClickFromStart}%)`;
-                            locationOnClickFromStart = maxDistanceFromStart;
-                            slide.style.opacity = 0;
-                            slide.style.transform = `translateX(${locationOnClickFromStart}%)`;
-                        }, 300);
                     }
+
+                    let counter = 0;
                     setTimeout(() => {
                         slide.style.transform = `translateX(${locationOnClickFromStart}%)`;
-                        slide.style.transition = `transform 0.2s ease-in-out`;
+                        slide.style.transition = `transform 0.25s ease-in-out`;
                         slide.style.opacity = 1;
+                        let firstInterval = setInterval(() => {
+                            counter++;
+                            if(counter >= 35) {
+                                clearInterval(firstInterval);
+                                sliderContainer.lastElementChild.style.opacity = 0;
+                                locationOnClickFromStart = maxDistanceFromStart;
+                                sliderContainer.lastElementChild.style.transform = `translateX(${locationOnClickFromStart}%)`;
+                            }
+                        }, 1);
                     }, 0);
                 });
             }
         }
 
-        buttons.forEach(btn => btn.addEventListener('click', debounce(moveSlides)));
+        buttons.forEach(btn => btn.addEventListener('click', moveSlides));
     }
 }
 export default Generator;
