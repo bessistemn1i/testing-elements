4ea57cd8a54a72489b13a1d75cb129a805eaec5a
bessistemn1i <eduard.zhulega@yandex.ru>
2019-05-16 17:00:33 +0200

    working hard

--- src/js/modules/Slider.js
+++ src/js/modules/Slider.js
@@ -6,36 +6,36 @@ export default class Slider {
         this.next = this.nav.querySelector('[data-path="next"]');
         this.sliderContainer = this.slider.querySelector('.slider__container');
         this.slides = [...document.querySelectorAll('.slide')];
-        this.counter = 0;
-        this.counterSlide = 0;
-        this.counterHandler = 0;
-        this.pos = 0;
         this.addEvents();
     }
 
     posSlides(arr) {
         arr.map((el, idx) => {
             const SLIDE_WIDTH = 300;
+            if(idx == 0) {
+                el.style.marginLeft = `-300px`;
+            }
             el.style.marginLeft = `${SLIDE_WIDTH * idx}px`;
+            el.style.display = `block`;
         });
     }
 
     moveBackward(arr) {
-        arr[0].style.background = 'red';
         const sliderNext = arr.slice(0, -1);
-        this.pos++;
         this.sliderContainer.insertBefore(sliderNext[0], arr[1]);
         arr[arr.length - 1].style.marginLeft = '-300px';
+        arr[arr.length - 1].style.display = 'none';
+
         setTimeout(() => {
             arr.unshift(arr.pop());
-            this.posSlides(sliderNext);
+            
+            //this.posSlides(sliderNext);
+            this.posSlides(arr);
         }, 10);
     }
 
-    moveForward(arr) {
-        arr[0].style.background = 'blue';
+    moveForward(evt, arr) {
         const newSlider = arr.slice(1);
-        this.pos--;
         
         this.sliderContainer.appendChild(newSlider[newSlider.length - 1]);
         arr[0].style.marginLeft = '-300px';
@@ -48,6 +48,6 @@ export default class Slider {
     addEvents() {
         this.posSlides(this.slides);
         this.prev.addEventListener('click', () => this.moveBackward(this.slides));
-        this.next.addEventListener("click", () =>this.moveForward(this.slides));
+        this.next.addEventListener("click", (event) =>this.moveForward(event, this.slides));
     }
 }
