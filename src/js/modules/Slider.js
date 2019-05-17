0bf6106c253bcc95261a6dfffe602bf6315daca3
bessistemn1i <eduard.zhulega@yandex.ru>
2019-05-17 13:19:46 +0200

    test slider on real items

--- src/js/modules/Slider.js
+++ src/js/modules/Slider.js
@@ -5,40 +5,51 @@ export default class Slider {
         this.prev = this.nav.querySelector('[data-path="prev"]');
         this.next = this.nav.querySelector('[data-path="next"]');
         this.sliderContainer = this.slider.querySelector('.slider__container');
-        this.slides = [...document.querySelectorAll('.slide')];
+        this.slides = [...document.querySelectorAll('.custom-carousel__slide')];
         this.addEvents();
     }
 
     posSlides(arr) {
         arr.map((el, idx) => {
             const SLIDE_WIDTH = 300;
-            if(idx == 0) {
+            if(idx == arr.length - 1) {
                 el.style.marginLeft = `-300px`;
+                el.style.display = 'none';
             }
             el.style.marginLeft = `${SLIDE_WIDTH * idx}px`;
             el.style.display = `block`;
         });
     }
 
-    moveBackward(arr) {
-        const sliderNext = arr.slice(0, -1);
-        this.sliderContainer.insertBefore(sliderNext[0], arr[1]);
-        arr[arr.length - 1].style.marginLeft = '-300px';
-        arr[arr.length - 1].style.display = 'none';
+    posSlidesBackward(arr) {
+        const SLIDE_WIDTH = 300;
+        arr[arr.length - 1].style.marginLeft = `-300px`;
+        
+        arr.map((el, idx) => {
+            if(idx !== arr.length - 1) {
+                el.style.marginLeft = `${SLIDE_WIDTH * idx}px`;
+                el.style.display = 'none';
+            }
+            el.style.display = `block`;
+        })
+    }
 
+    moveBackward(arr) {
+        this.sliderContainer.insertBefore(arr[0], arr[1]);
+        arr[arr.length - 2].style.display = 'none';
         setTimeout(() => {
             arr.unshift(arr.pop());
-            
-            //this.posSlides(sliderNext);
-            this.posSlides(arr);
+            this.posSlidesBackward(arr);
         }, 10);
     }
 
-    moveForward(evt, arr) {
+    moveForward(arr) {
         const newSlider = arr.slice(1);
         
         this.sliderContainer.appendChild(newSlider[newSlider.length - 1]);
         arr[0].style.marginLeft = '-300px';
+        arr[arr.length - 1].style.display = 'none';
+
         setTimeout(() => {
             arr.push(arr.shift());
             this.posSlides(newSlider);
@@ -48,6 +59,6 @@ export default class Slider {
     addEvents() {
         this.posSlides(this.slides);
         this.prev.addEventListener('click', () => this.moveBackward(this.slides));
-        this.next.addEventListener("click", (event) =>this.moveForward(event, this.slides));
+        this.next.addEventListener("click", () =>this.moveForward(this.slides));
     }
 }
