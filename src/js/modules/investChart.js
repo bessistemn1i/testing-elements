7ef2dd77be9410c68b51df94352c698561df45c9
bessistemn1i <eduard.zhulega@yandex.ru>
2019-08-21 17:01:23 +0200

    complete graph

--- src/js/modules/investChart.js
+++ src/js/modules/investChart.js
@@ -8,7 +8,6 @@ class InvestChart {
     addEvents() {
         const investChart = new Chart(this.ctx, {
             type: 'line',
-            
             data: {
                 labels: ['1 месяц', '1 год', '5 лет'],
                 datasets: [{
@@ -19,7 +18,8 @@ class InvestChart {
                     borderWidth: 1,
                     borderColor: "#d6a354",
                     pointBackgroundColor: "#d6a354",
-                    pointBorderWidth: 1
+                    pointBorderWidth: 1,
+                    pointStyle: 'rect'
                 },
                 {
                     label: 'Доля в размере 20%',
@@ -29,7 +29,8 @@ class InvestChart {
                     borderWidth: 1,
                     borderColor: "#d6a354",
                     pointBackgroundColor: "#d6a354",
-                    pointBorderWidth: 1
+                    pointBorderWidth: 1,
+                    pointStyle: 'rect'
                 },
                 {
                     label: 'Доля в размере 30%',
@@ -39,7 +40,8 @@ class InvestChart {
                     borderWidth: 1,
                     borderColor: "#d6a354",
                     pointBackgroundColor: "#d6a354",
-                    pointBorderWidth: 1
+                    pointBorderWidth: 1,
+                    pointStyle: 'rect'
                 },
                 {
                     label: 'Доля в размере 40%',
@@ -49,7 +51,8 @@ class InvestChart {
                     borderWidth: 1,
                     borderColor: "#d6a354",
                     pointBackgroundColor: "#d6a354",
-                    pointBorderWidth: 1
+                    pointBorderWidth: 1,
+                    pointStyle: 'rect'
                 },
                 {
                     label: 'Доля в размере 50%',
@@ -59,28 +62,28 @@ class InvestChart {
                     borderWidth: 1,
                     borderColor: "#d6a354",
                     pointBackgroundColor: "#d6a354",
-                    pointBorderWidth: 1
+                    pointBorderWidth: 1,
+                    pointStyle: 'rect'
                 },
             ],
             },
+            
             options: {
                 legend: {
                     display: true,
                     position: 'top',
                     labels: {
-                        fontColor: '#333'
+                        fontColor: '#131313',
+                        fontSize: 14,
+                        usePointStyle: true
                     },
-                    tooltips: {
-                        custom: (tooltip) => {
-                            if(!tooltip.opacity) {
-                                document.querySelector('#canvas2017').style.cursor = 'default';
-                                return;
-                            }
+                    
+                    onHover: (event, legendItem) => {
+                        if(legendItem) {
+                          event.srcElement.style.cursor = 'pointer';
                         }
                     },
-                    onHover: (event, legendItem) => {
-                        document.querySelector('#canvas2017').style.cursor = 'pointer'
-                    },
+
                     onClick: function(e, legendItem) {
                         var index = legendItem.datasetIndex;
                         var ci = this.chart;
@@ -130,17 +133,114 @@ class InvestChart {
                         ci.update();
                       },
                 },
+
+                onHover: (event) => {
+                  event.srcElement.style.cursor = 'default';
+                },
+
+                tooltips: {
+                  // Disable the on-canvas tooltip
+                    enabled: false,
+                    intersect: false,
+                    custom: function(tooltipModel) {
+                        // Tooltip Element
+                        var tooltipEl = document.getElementById('chartjs-tooltip');
+                        // Create element on first render
+                        if (!tooltipEl) {
+                            tooltipEl = document.createElement('div');
+                            tooltipEl.id = 'chartjs-tooltip';
+                            tooltipEl.innerHTML = '<table></table>';
+                            tooltipEl.style.display = 'block';
+                            tooltipEl.style.visibility = 'visible';
+                            tooltipEl.style.opacity = 1;
+                            tooltipEl.style.background = 'rgba(255, 255, 255, 1)';
+                            tooltipEl.style.border = '1px solid #d6a354';
+                            document.body.appendChild(tooltipEl);
+                        }
+
+                        // Hide if no tooltip
+                        if (tooltipModel.opacity === 0) {
+                            tooltipEl.style.opacity = 0;
+                            return;
+                        }
+
+                        // Set caret Position
+                        if (tooltipModel.yAlign) {
+                            tooltipEl.classList.add(tooltipModel.yAlign);
+                        } else {
+                            tooltipEl.classList.add('no-transform');
+                        }
+
+                        function getBody(bodyItem) {
+                            return bodyItem.lines;
+                        }
+
+                        // Set Text
+                        if (tooltipModel.body) {
+                          var titleLines = tooltipModel.title || [];
+                          var bodyLines = tooltipModel.body.map(getBody);
+                          const fixedBodyLines = bodyLines.map((el) => {
+                            const ArOfStr = el[0].split(' ');
+                            const num = (ArOfStr[ArOfStr.length - 1]);
+                            const regexp = /\d{1,3}(?=(\d{3})+(?!\d))/g;
+                            const rNum = num.replace(regexp, '$& ');
+                            ArOfStr[ArOfStr.length - 1] = rNum;
+                            const result = ArOfStr.join(' ');
+                            return result;
+                          });
+                            var innerHtml = '<thead>';
+
+                            titleLines.forEach(function(title) {
+                                innerHtml += '<tr><th>' + title + '</th></tr>';
+                            });
+                            innerHtml += '</thead><tbody>';
+
+                            fixedBodyLines.forEach(function(body, i) {
+                                var colors = tooltipModel.labelColors[i];
+                                var style = 'background:' + colors.backgroundColor;
+                                style += '; border-color:' + colors.borderColor;
+                                style += '; border-width: 2px';
+                                var span = '<span style="' + style + '"></span>';
+                                innerHtml += '<tr><td>' + span + body + '</td></tr>';
+                            });
+                            innerHtml += '</tbody>';
+
+                            var tableRoot = tooltipEl.querySelector('table');
+                            tableRoot.innerHTML = innerHtml;
+                        }
+
+                        // `this` will be the overall tooltip
+                        var position = this._chart.canvas.getBoundingClientRect();
+                        // Display, position, and set styles for font
+                        tooltipEl.style.opacity = 1;
+                        tooltipEl.style.position = 'absolute';
+                        if(tooltipModel.caretX > 700) {
+                          tooltipEl.style.left = tooltipModel.caretX + position.left - 200 + 'px';
+                        }
+                        else {
+                          tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+                        }
+                        tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+                        tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
+                        tooltipEl.style.fontSize = tooltipModel.bodyFontSize + 'px';
+                        tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
+                        tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+                        tooltipEl.style.pointerEvents = 'none';
+                    },
+                },
                 scales: {
                   yAxes: [
                     {
                       ticks: {
                         callback: (label, index, labels) => {
-                          console.log(label);
                           if(label/1000 >= 1000) {
                             return label/1000000+' млн.'
                           }
-                          else {
+                          else if (label/1000 < 1000 && label/1000 >= 100) {
                             return label/1000+' тыс.';
+                          }
+                          else if (label/1000 < 100) {
+                            return label+' тыс.'
                           }
                         },
                         scaleLabel: {
